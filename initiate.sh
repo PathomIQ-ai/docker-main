a3c299ceebbfcff4fd16d20b0526934f82c919d9
ml-and-ml <hassan.m.eng@gmail.com>
2022-10-26 05:37:18 +0000

    login

--- initiate.sh
+++ initiate.sh
@@ -15,7 +15,7 @@ mkdir /home/ec2-user/download/
 
 if [ ${MODE} == 'train' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/train.py --tissue ${TISSUE} --dataset ${DATASET} --net_type ${NET_TYPE} --batch ${BATCH_SIZE} \
     --lr ${LEARNING_RATE} --train_sample ${TRAIN_SAMPLE} --valid_sample ${VALID_SAMPLE} --milestone ${MILESTONE} --epochs ${EPOCHS} \
     --workers ${WORKERS} --pretrain_experiment ${PRETRAIN_EXPERIMENT} --pretrain_run ${PRETRAIN_RUN} --pretrain_tissue ${PRETRAIN_TISSUE} \
@@ -24,14 +24,14 @@ fi
 
 if [ ${MODE} == 'predict' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/predict.py --dataset ${DATASET} --tissue ${TISSUE} --experiment ${EXPERIMENT} --slide ${SLIDE} \
     --checkpoint ${CHECKPOINT} --run ${RUN} --batch ${BATCH} --workers ${WORKERS} --net_type ${NET_TYPE}
 fi
 
 if [ ${MODE} == 'predict_full_pipeline' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/predict_pipeline.py --slide ${SLIDE} \
     --stain_net_exp ${STAIN_NET_EXP} --stain_net_run ${STAIN_NET_RUN} --stain_net_epoch ${STAIN_NET_EPOCH} \
     --epithelial_net_exp ${EPITHELIAL_NET_EXP} --epithelial_net_run ${EPITHELIAL_NET_RUN} --epithelial_net_epoch ${EPITHELIAL_NET_EPOCH} \
@@ -41,13 +41,13 @@ fi
 
 if [ ${MODE} == 'feature_extract' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/lung_features.py --slide ${SLIDE} --pretrain_epoch ${PRETRAIN_EPOCH} --dataset ${DATASET} --pretrain_run ${PRETRAIN_RUN} --pretrain_exp ${PRETRAIN_EXP}
 fi
 
 if [ ${MODE} == 'cluster' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/cluster.py --dataset ${DATASET} --experiment ${EXPERIMENT} --checkpoint ${CHECKPOINT} --run ${RUN} --tissue ${TISSUE}
 fi
 
@@ -55,14 +55,14 @@ fi
 if [ ${MODE} == 'local_features' ] 
 then
     cd /home/ec2-user/download
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/lung-response.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/lung-response.git /home/ec2-user/download/
     python /home/ec2-user/download/features_batch_job.py --radius ${RADIUS}
 fi
 
 
 if [ ${MODE} == 'prostate_features' ] 
 then
-    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
+    git clone https://ml-and-ml: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@github.com/PathomIQ-ai/CNN-train-predict.git /home/ec2-user/download/
     python /home/ec2-user/download/prostate_features.py --pretrain_exp ${PRETRAIN_EXP} --slide ${SLIDE} \
     --pretrain_epoch ${PRETRAIN_EPOCH} --pretrain_run ${PRETRAIN_RUN} --dataset ${DATASET}
 fi
